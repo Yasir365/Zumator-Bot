0512d5f43c423126fa732ebe51303e50efba700a
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-09-27 18:22:28 +0500

    updated

--- src/pages/base/Base.jsx
+++ src/pages/base/Base.jsx
@@ -1,13 +1,27 @@
+import { useState } from 'react';
 import './base.scss';
 
 
 export default function Base() {
+    const [value, setValue] = useState(0);
+
     return (
         <div className='base'>
             <h1 className="title">Base</h1>
 
-            <img className='img' src="/images/img3.jpg" alt="" />
-
+            <div className='d-flex justify-content-center align-items-center'>
+                <img
+                    className='glow-on-click'
+                    src="/images/logo.ico"
+                    alt="coin"
+                    onClick={() => setValue(value + 1)}
+                />
+            </div>
+            <h1 className="mt-5 text-center value">
+                <span className={value < 10 ? 'text-danger' : value < 20 ? 'text-warning' : 'text-success'}>
+                    {value}
+                </span>
+            </h1>
         </div>
     )
 }
