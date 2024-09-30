354cbf39f3e1ac14d70da82a288627154092c2e2
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-09-30 17:11:28 +0500

    ops

--- src/pages/base/Base.jsx
+++ src/pages/base/Base.jsx
@@ -1,39 +1,13 @@
-import { useState, useEffect } from 'react';
 import './base.scss';
 
 
 export default function Base() {
-    const [value, setValue] = useState(0);
-
-    useEffect(() => {
-        const storedValue = localStorage.getItem('value');
-        if (storedValue) {
-            setValue(+storedValue);
-        }
-    }, []);
-
-    const handleTap = () => {
-        setValue((prevValue) => prevValue + 1);
-        localStorage.setItem('value', String(value + 1));
-    }
 
     return (
         <div className='base'>
             <h1 className="title">Base</h1>
 
-            <div className='d-flex justify-content-center align-items-center'>
-                <img
-                    className='glow-on-click'
-                    src="/images/logo.ico"
-                    alt="coin"
-                    onClick={handleTap}
-                />
-            </div>
-            <h1 className="mt-5 text-center value">
-                <span className={value < 10 ? 'text-danger' : value < 20 ? 'text-warning' : 'text-success'}>
-                    {value}
-                </span>
-            </h1>
+
         </div>
     )
 }
--- src/pages/ops/Ops.jsx
+++ src/pages/ops/Ops.jsx
@@ -1,10 +1,384 @@
 import './ops.scss'
+import { useState } from 'react'
+import moment from 'moment'
 
 export default function Ops() {
+  const [date, setDate] = useState(new Date())
+  const [activeTab, setActiveTab] = useState(1)
+
+  const formattedDate = moment(date).format('YYYY-MM-DD')
   return (
     <div className='ops'>
-      <h1 className="title">Ops</h1>
-      <img className='img' src="/images/img5.jpg" alt="" />
+
+      <div className='date'>{formattedDate}</div>
+
+      <div className='combo'>
+        <div>Daily Cambo</div>
+        <div>0.0000</div>
+      </div>
+
+      <div className='images-container'>
+        <div className='image'>
+          <img src="/images/img1.jpg" alt="" />
+        </div>
+        <div className='image'>
+          <img src="/images/img2.jpg" alt="" />
+        </div>
+        <div className='image'>
+          <img src="/images/img3.jpg" alt="" />
+        </div>
+        <div className='image'>
+          <img src="/images/img4.jpg" alt="" />
+        </div>
+      </div>
+
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <a
+            className={activeTab === 1 ? "nav-link active" : "nav-link"}
+            aria-current="page"
+            href="#"
+            onClick={() => setActiveTab(1)}
+          >
+            Web 3
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            className={activeTab === 2 ? "nav-link active" : "nav-link"}
+            aria-current="page"
+            href="#"
+            onClick={() => setActiveTab(2)}
+          >
+            Market
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            className={activeTab === 3 ? "nav-link active" : "nav-link"}
+            aria-current="page"
+            href="#"
+            onClick={() => setActiveTab(3)}
+          >
+            Galatic
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            className={activeTab === 4 ? "nav-link active" : "nav-link"}
+            aria-current="page"
+            href="#"
+            onClick={() => setActiveTab(4)}
+          >
+            Aritifacts
+          </a>
+        </li>
+      </ul>
+
+      {activeTab === 1 && (
+        <div className='tab-content'>
+          <div className='left'>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img1.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img1.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img1.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img1.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>
+            </div>
+          </div>
+          <div className='left'>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img1.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img1.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img1.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img1.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+      )}
+      {activeTab === 2 && (
+        <div className='tab-content'>
+          <div className='left'>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img2.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img2.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img2.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img2.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>
+            </div>
+          </div>
+          <div className='left'>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img2.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img2.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img2.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img2.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+      )}
+      {activeTab === 3 && (
+        <div className='tab-content'>
+          <div className='left'>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img3.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img3.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img3.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img3.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>
+            </div>
+          </div>
+          <div className='left'>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img3.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img3.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img3.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img3.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+      )}
+      {activeTab === 4 && (
+        <div className='tab-content'>
+          <div className='left'>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img4.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img4.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img4.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img4.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>
+            </div>
+          </div>
+          <div className='left'>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img4.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img4.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img4.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>            </div>
+            <div className="item">
+              <div className='card-image'>
+                <img src="/images/img4.jpg" alt="" />
+              </div>
+              <div className='card-info'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo fugit soluta alias.</p>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+      )}
 
     </div>
   )
