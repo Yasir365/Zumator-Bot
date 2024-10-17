ec5c90fc705889dc124001aef9ad9c6f3f0febd7
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-10-17 11:58:32 +0500

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -32,7 +32,7 @@ export default function Header() {
             <div className="profile-container">
                 {userInfo ? (
                     <>
-                        <img src={userInfo.photoUrl} alt="" lazyload="true" />
+                        <img src={userInfo.photoUrl} alt="img" lazyload="true" />
                         <p className="title">
                             {userInfo.firstName} {userInfo.lastName} <small>(Conqueror)</small>
                         </p>
