2b7d0bcb4c2edb4b2074272c77da4e8e33520044
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-10-21 19:27:10 +0500

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import Header from "./components/header/Header";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 
@@ -6,9 +5,7 @@ import Navbar from "./components/navbar/Navbar";
 export default function App() {
     return (
         <>
-            <div className="outlet-container">
-                <Outlet />
-            </div>
+            <Outlet />
             <Navbar />
         </>
     )
