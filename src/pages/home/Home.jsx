60e3a0f91acbc2f846f1deb43cc8aab451e8de14
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-10-29 16:33:40 +0500

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -21,13 +21,11 @@ export default function Home() {
     }, []);
 
     useEffect(() => {
-        const hash = window.location.href;
-        console.log("Telegram ID ----------- :: ", hash);
-
-        // if (hash) {
-        //     const id = hash.substring(1); 
-        // }
-
+        const hash = location.hash; // Get the hash part of the URL
+        const id = hash ? hash.substring(hash.indexOf('frndId') + 6) : null;
+    
+        console.log("Telegram ID ----------- :: ", id);
+        
         if (ref) {
             let data = JSON.parse(localStorage.getItem('userInfo'))
             if (data) {
