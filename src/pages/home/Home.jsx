b1490a1279abd222992631527e37585074a64f33
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-10-29 12:35:56 +0500

    udpated

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -71,7 +71,7 @@ export default function Home() {
                     </div>
 
                     <div className="item">
-                        <Link to="/rewards" onClick={updateCloseButton('other')}>
+                        <Link to="/rewards">
                             <div className="img"> <img src="/images/daily-board/3.webp" alt="img" /> </div>
                             <p>Reward</p>
                         </Link>
