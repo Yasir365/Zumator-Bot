71fd5b4554b018e58a3a7d3a39c02c5e4b93d15f
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-10-29 11:21:19 +0500

    udpated

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,6 @@ import { useEffect } from "react";
 import { fetchUserInfo } from "./services/api.service";
 
 export default function App() {
-    let tg;
-    if (window.Telegram && window.Telegram.WebApp) {
-        tg = window.Telegram.WebApp;
-    }
-
-    function updateButton() {
-        if (window.Telegram && window.Telegram.WebApp) {
-            console.log("window.location.pathname", window.location.pathname);
-            
-            if (window.location.pathname != "OPS" || window.location.pathname != "ship-yard" || window.location.pathname != "friends" ||
-                window.location.pathname != "arena" || window.location.pathname != "leaderboard" || window.location.pathname != "settings" ||
-                window.location.pathname != "privacy-policy" || window.location.pathname != "change-language" || window.location.pathname != "rewards"
-            ) {
-                tg.BackButton.hide();
-                tg.MainButton.show().setText("Close").onClick(() => {
-                    tg.close();
-                });
-            } else {
-                tg.MainButton.hide();
-                tg.BackButton.show().onClick(() => {
-                    window.history.back();
-                });
-            }
-        }
-    }
-
-    updateButton();
-    window.addEventListener("popstate", updateButton);
-
     useEffect(() => {
         fetchUserInfo()
             .then(info => {
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,41 @@
 import './navbar.scss';
 import { NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import { useEffect } from 'react';
 
 export default function Navbar() {
   const { t } = useTranslation();
+  let tg;
+  if (window.Telegram && window.Telegram.WebApp) {
+      tg = window.Telegram.WebApp;
+  }
+
+  function updateButton() {
+      if (window.Telegram && window.Telegram.WebApp) {
+          console.log("window.location.pathname", window.location.pathname);
+          
+          if (window.location.pathname != "OPS" || window.location.pathname != "ship-yard" || window.location.pathname != "friends" ||
+              window.location.pathname != "arena" || window.location.pathname != "leaderboard" || window.location.pathname != "settings" ||
+              window.location.pathname != "privacy-policy" || window.location.pathname != "change-language" || window.location.pathname != "rewards"
+          ) {
+              tg.BackButton.hide();
+              tg.MainButton.show().setText("Close").onClick(() => {
+                  tg.close();
+              });
+          } else {
+              tg.MainButton.hide();
+              tg.BackButton.show().onClick(() => {
+                  window.history.back();
+              });
+          }
+      }
+  }
+  useEffect(() => {
+      updateButton();
+  }, [window.location.pathname]);
+
+  updateButton();
+  window.addEventListener("popstate", updateButton);
 
   return (
     <header className='user-navbar'>
@@ -18,7 +50,7 @@ export default function Navbar() {
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")} aria-current="page" to="/OPS">
+                <NavLink className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")} aria-current="page" to="/OPS" onClick={updateButton}>
                   <img src="/images/icons/gaming.webp" alt="" lazyload="true" />
                   Ops
                 </NavLink>
