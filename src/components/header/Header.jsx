e8b64b1eba655361068b831395adc431ea1f850d
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-10-30 11:17:50 +0500

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,21 +3,19 @@ import { Link } from 'react-router-dom';
 import { updateCloseButton } from '../../services/api.service';
 
 export default function Header() {
-    // const userInfo = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null;
-
-
+    const userInfo = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null;
 
     return (
         <header className="user-header">
             <div className="d-flex align-items-center">
                 <img src='/images/profile.webp' alt="img" lazyload="true" />
-                {/* {userInfo ? (
+                {userInfo ? (
                     <>
                         <p className="title"> {userInfo.first_name} {userInfo.last_name} <small>(Conqueror)</small> </p>
                     </>
                 ) : (
-                )} */}
-                <p className="title">Jane Cooper <small>(CEO)</small></p>
+                    <p className="title">Jane Cooper <small>(CEO)</small></p>
+                )}
             </div>
             <div className="setting">
                 <Link to="/settings" onClick={updateCloseButton('other')}>
