40f116580d18d414a22f7e04d01cf60a9d92ebd1
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-10-31 11:39:28 +0500

    Update Friends.jsx

--- src/pages/friends/Friends.jsx
+++ src/pages/friends/Friends.jsx
@@ -89,7 +89,7 @@ export default function Friends() {
 
       <div className="button-container">
         <button className='invite' onClick={handleInviteClick}>Send Invite</button>
-        <button className='copy' onClick={generateInviteLink}><i className="fa-regular fa-copy"></i></button>
+        <button className='copy' onClick={copyLink}><i className="fa-regular fa-copy"></i></button>
       </div>
     </div>
   )
