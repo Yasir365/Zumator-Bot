b3c911547abc0e763653fa274f51665279a084fa
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-11-06 14:32:32 +0500

    Update api.service.js

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseUrl = "http://localhost:3000/api/zumator-bot/v1"
-// const baseUrl = "https://zumbator-bot-backend.vercel.app/api/zumator-bot/v1"
+// const baseUrl = "http://localhost:3000/api/zumator-bot/v1"
+const baseUrl = "https://zumbator-bot-backend.vercel.app/api/zumator-bot/v1"
 
 export async function registerUser(data) {
     if (data) {
