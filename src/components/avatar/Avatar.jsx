847e4b2af6f65cbd2bddb9609b8dd518a4e438b2
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-11-07 16:57:43 +0500

    add avatar 3 modal

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,9 +1,28 @@
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useGLTF } from '@react-three/drei';
+
+
+function Model() {
+    const { scene } = useGLTF('/modal/bot.glb');
+    return <primitive object={scene} scale={[0.1, 0.1, 0.1]} />;
+}
 
 export default function Avatar() {
     return (
         <div className="main-image-wrapper">
             <div className="image">
-                <img src="/images/hero.webp" alt="" />
+                {/* <img src="/images/hero.webp" alt="" /> */}
+                <div style={{ width: '100%', height: '300px', }}>
+                    <Canvas>
+                        <ambientLight intensity={0.5} />
+                        <directionalLight position={[5, 5, 5]} />
+                        <Suspense fallback={null}>
+                            <Model />
+                        </Suspense>
+                        <OrbitControls enableZoom={true} />
+                    </Canvas>
+                </div>
             </div>
         </div>
     )
