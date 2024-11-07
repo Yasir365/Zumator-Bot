c084ef022c8a7c2813e663b9c0c0c6aec0e915b7
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-11-07 19:06:01 +0500

    udpated

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -12,7 +12,7 @@ export default function Avatar() {
     return (
         <div className="main-image-wrapper">
             <div className="model">
-                <div style={{ width: '100%', height: '300px', }}>
+                <div style={{ width: '100%', height: '250px', }}>
                     <Canvas>
                         <ambientLight intensity={0} />
                         <directionalLight position={[0, 0, 0]} />
