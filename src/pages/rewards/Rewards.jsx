e74aa76f31ea9a436adeab1255faa9706ec82261
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-11-07 18:04:01 +0500

    Update Rewards.jsx

--- src/pages/rewards/Rewards.jsx
+++ src/pages/rewards/Rewards.jsx
@@ -31,7 +31,9 @@ export default function Rewards() {
   const handleClaim = async () => {
     if (!isClaimed) {
       const data = await updatePoints({ _id: userInfo._id, points: 100 });
-      if (data) {
+      console.log(":::::::::::", data);
+      
+      if (data.success) {
         dispatch(saveUser(data));
         swalToastr('success', 'Daily reward claimed!');
         setRewardClaimed();
