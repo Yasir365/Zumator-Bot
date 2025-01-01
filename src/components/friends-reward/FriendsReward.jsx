72d91abeb957a1b70c6594cea0e9c768d7af865c
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2025-01-01 12:47:10 +0500

    FIX LOADER

--- src/components/friends-reward/FriendsReward.jsx
+++ src/components/friends-reward/FriendsReward.jsx
@@ -37,23 +37,30 @@ export default function FriendsReward() {
     }
 
     return (
-        <div className="tab-content">
-            {data.map((item, index) => (
-                <div className='w-100' key={index}>
-                    <div className="d-flex justify-content-between align-items-center pb-1">
-                        <div className='d-flex align-items-center'>
-                            <img src="images/icons/friends.webp" alt="" />
-                            <div className='ms-2'>
-                                <p className='bold'>{item.title}</p>
-                                <p className='small'><img src="/images/icons/usdt.webp" alt="" /> +{item.coins}</p>
-                            </div>
-                        </div>
+        <>
+            {!fetchloader && data.length == 0 && <div className="api-loader"><h5 className="text-white">No Data Found</h5></div>}
+            {!fetchloader && data.length > 0 &&
+                <div className="tab-content">
+                    {data.map((item, index) => (
+                        <div className='w-100' key={index}>
+                            <div className="d-flex justify-content-between align-items-center pb-1">
+                                <div className='d-flex align-items-center'>
+                                    <img src="images/icons/friends.webp" alt="" />
+                                    <div className='ms-2'>
+                                        <p className='bold'>{item.title}</p>
+                                        <p className='small'><img src="/images/icons/usdt.webp" alt="" /> +{item.coins}</p>
+                                    </div>
+                                </div>
 
-                        <button className={!item.is_ready_to_claim || item.is_claimed ? "start disable" : "start"} disabled={!item.is_ready_to_claim || item.is_claimed} onClick={() => claimReward(item.id)}>{t('Claim')}</button>
-                    </div>
-                    {index !== data.length - 1 && (<hr />)}
+                                <button className={!item.is_ready_to_claim || item.is_claimed ? "start disable" : "start"} disabled={!item.is_ready_to_claim || item.is_claimed} onClick={() => claimReward(item.id)}>{t('Claim')}</button>
+                            </div>
+                            {index !== data.length - 1 && (<hr />)}
+                        </div>
+                    ))}
                 </div>
-            ))}
-        </div>
+            }
+            {fetchloader && <div className="api-loader"><div className="spinner-border" role="status"></div></div>}
+        </>
+
     )
 }
