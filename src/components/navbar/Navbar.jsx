06a222ad65796805d1fc576dabbd7dc064009771
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2025-01-02 11:38:32 +0500

    update icons

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,25 +14,25 @@ export default function Navbar() {
             <ul className="navbar-nav d-flex flex-row justify-content-evenly">
               <li className="nav-item">
                 <NavLink className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")} aria-current="page" to="/">
-                  <img src="/images/icons/frog.webp" alt="" />
+                  <img src="/images/icons/frog.png" alt="" />
                   {t('Base')}
                 </NavLink>
               </li>
               <li className="nav-item">
                 <NavLink className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")} aria-current="page" to="/OPS">
-                  <img src="/images/icons/gaming.webp" alt="" />
+                  <img src="/images/icons/gaming.png" alt="" />
                   {t('Ops')}
                 </NavLink>
               </li>
               <li className="nav-item">
                 <NavLink className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")} aria-current="page" to="/space-port">
-                  <img src="/images/icons/shipyard.webp" alt="" />
+                  <img src="/images/icons/shipyard.png" alt="" />
                   {t('Space-Port')}
                 </NavLink>
               </li>
               <li className="nav-item">
                 <NavLink className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")} aria-current="page" to="/friends">
-                  <img src="/images/icons/friends.webp" alt="" />
+                  <img src="/images/icons/friends.png" alt="" />
                   {t('Friends')}
                 </NavLink>
               </li>
