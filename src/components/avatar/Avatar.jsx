dcc5be5909803bc02cedd2d5bba218e42392e1ee
Malik Amir <142987219+Malik-Amir30@users.noreply.github.com>
2025-01-02 13:07:06 +0500

    new changes

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -17,7 +17,7 @@ export default function Avatar() {
     return (
         <div className="main-image-wrapper">
             <div className="model">
-                <div style={{ width: '100%', height: '192px', }}>
+                <div style={{ width: '100%', height: 'auto', }}>
                     <img src="/images/hero.png" alt="" />
                     {/* <Canvas>
                         <ambientLight intensity={0} />
